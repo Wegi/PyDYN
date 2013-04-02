d1ce1b73d0455d6095b84ce8e7c838aa2710c384
Alexander Schneider <wegi.pwnz@googlemail.com>
2013-04-02 16:19:12 +0200

    rework how api works. add installFor() method that is now mandatory to use before other methods

--- pydyn.py
+++ pydyn.py
@@ -5,6 +5,7 @@ This way you'll have safe calls and won't have to deal with changing
 requirements/names/formats.
 All methods asume you have set the solver and working set accordingly to your
 wishes / configuration.
+Use the installationFor() Method firsthand.
 """
 
 import solver
@@ -14,8 +15,22 @@ from pkg_resources import WorkingSet
 
 _solver = 'minisat+'
 _working_set = None
+_installList = []
+_uninstallList = []
 
-solver.loadCache()
+def installFor(name):
+    """Set the parameter for the module which you want to install.
+
+    Use this function (once) before calling drawXY() / getInstallLists() / getInstallStrings() / etc.
+    """
+    solver.loadCache()
+    with open('pydyn.opb', 'w') as f:
+        f.write(solver.generateOPB(name, working_set=_working_set if _working_set 
+            else solver.generateOPB.__defaults__[0]))
+    solver.saveCache()
+    global _installList
+    global _uninstallList
+    _installList, _uninstallList = solver.parseSolverOutput(solver._callSolver('pydyn.opb', solver=_solver))
 
 def loadCache(path):
     """Load another cache file."""
@@ -29,13 +44,9 @@ def getInstallLists(name):
     """Get two lists of which modules to install or uninstall.
 
     Returns two lists install, uninstall. 
-    The Content of the Lists ist a tuple with (name, version).
+    The Content of the Lists is a tuple with (name, version).
     """
-    with open('pydyn.opb', 'w') as f:
-        f.write(solver.generateOPB(name, working_set=_working_set if _working_set 
-            else solver.generateOPB.__defaults__[0]))
-    solver.saveCache()
-    return solver.parseSolverOutput(solver._callSolver('pydyn.opb', solver=_solver))
+    return _installList, _uninstallList
 
 def setSolver(solver):
     """Set the opb-solver to use. (standard is minisat+)
@@ -48,23 +59,30 @@ def setSolver(solver):
 def setWorkingSet(paths):
     """Set the Working Set to paths.
 
+    The Working Set is the active module path where your modules for a 
+    project reside.
     paths has to be a list of paths.
     """
     _working_set = WorkingSet(paths)
 
 def setDefaultWorkingSet():
-    """Set the Working Set back to default."""
+    """Set the Working Set back to default (sys.path).
+
+    The Working Set is the active module path where your modules for a 
+    project reside.
+    """
     _working_set = None
 
-def getInstallStrings(install, uninstall):
-    """Parses the output of getInstallLists.
+def getInstallStrings():
+    """Parses the output of installFor().
 
     Get a Human Readable String which gives Install / uninstall advice.
     """
-    solver.installRecommendation(install, uninstall)
+    solver.installRecommendation(_installList, _uninstallList)
 
-def drawPNG(name):
-    """Output a .png file with the dependency graph after name would be installed."""
-    i, u = getInstallLists(name)
-    graph = solver.getFutureState(i)
+def drawPNG():
+    """Output a .png file with the dependency graph after parsed module would be installed.
+
+    A module should have been parsed with installFor() before calling this function"""
+    graph = solver.getFutureState(_installList)
     depgraph.graphToPNG(graph)
