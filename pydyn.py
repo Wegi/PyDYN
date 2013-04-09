7907024c7482b333ad283f5bd047d0abd4e9b2dd
Alexander Schneider <wegi.pwnz@googlemail.com>
2013-04-09 15:01:26 +0200

    Refactor api to use Dependency-Instance Objects now

--- pydyn.py
+++ pydyn.py
@@ -5,107 +5,94 @@ This way you'll have safe calls and won't have to deal with changing
 requirements/names/formats.
 All methods asume you have set the solver and working set accordingly to your
 wishes / configuration.
-Use the installationFor() Method firsthand.
+Create a DepInstance Object first if you want to work with dependencys and not just check it.
 """
 
 import solver
 import depgraph
+import pkg_resources
 
-from pkg_resources import WorkingSet
+class DepInstance:
 
-_solver = './wbo/wbo'
-_solvoptions = ['-file-format=opb']
-_working_set = None
-_installList = []
-_uninstallList = []
-_solvable = True
+    def __init__(self, name, solverprog='./wbo/wbo', solverOptions=['-file-format=opb'], wset=None):
+        """Set the parameter for the module which you want to install."""
 
-def installFor(name):
-    """Set the parameter for the module which you want to install.
+        solver.loadCache()
+        self.solverprog = solverprog
+        self.solverOptions = solverOptions
+        if wset: self.wset = wset
+        else: self.wset = pkg_resources.working_set
 
-    Use this function (once) before calling drawXY() / getInstallLists() / getInstallStrings() / etc.
-    """
-    solver.loadCache()
-    reqdict, version = solver.generateMetadata(name)
-    with open('pydyn.opb', 'w') as f:
-        f.write(solver.generateOPB(reqdict, name, version, working_set=_working_set if _working_set 
-            else solver.generateOPB.__defaults__[0]))
-    solver.saveCache()
-    global _installList
-    global _uninstallList
-    global _solvable
-    _installList, _uninstallList, _solvable = \
-    solver.parseSolverOutput(solver._callSolver('pydyn.opb', solver=_solver, options=_solvoptions))
+        reqdict, version = solver.generateMetadata(name)
+        with open('pydyn.opb', 'w') as f:
+            f.write(solver.generateOPB(reqdict, name, version, working_set=self.wset))
+        solver.saveCache() 
+        self.installList, self.uninstallList, self.solvable = \
+        solver.parseSolverOutput(solver._callSolver('pydyn.opb', solver=self.solverprog, options=self.solverOptions))
 
-def loadCache(path):
-    """Load another cache file."""
-    solver.loadCache(path)
+    def getInstallLists(self):
+        """Get two lists of which modules to install or uninstall.
 
-def saveCache(path):
-    """Save the cache to another file than pydyn.cache"""
-    solver.saveCache(path)
+        Returns two lists install, uninstall. 
+        The Content of the Lists is a tuple with (name, version).
+        """
+        return self.installList, self.uninstallList
 
-def getInstallLists(name):
-    """Get two lists of which modules to install or uninstall.
+    def setSolver(self, solverprog, solverOptions=[]):
+        """Set the opb-solver to use. (standard is minisat+)
 
-    Returns two lists install, uninstall. 
-    The Content of the Lists is a tuple with (name, version).
-    """
-    return _installList, _uninstallList
+        The Solver has to accept the opb Format and generate 
+        his output according to DIMACS regulations as of 2013.
+        """
 
-def setSolver(solver):
-    """Set the opb-solver to use. (standard is minisat+)
+        self.solverprog = solverprog
+        self.solverOptions = solverOptions
 
-    The Solver has to accept the opb Format and generate 
-    his output according to DIMACS regulations as of 2013.
-    """
-    _solver = solver
+    def setWorkingSet(self, paths):
+        """Set the Working Set to paths.
 
-def setWorkingSet(paths):
-    """Set the Working Set to paths.
+        The Working Set is the active module path where your modules for a 
+        project reside.
+        paths has to be a list of paths.
+        """
+        self.wset = pkg_resources.WorkingSet(paths)
 
-    The Working Set is the active module path where your modules for a 
-    project reside.
-    paths has to be a list of paths.
-    """
-    _working_set = WorkingSet(paths)
+    def setDefaultWorkingSet(self):
+        """Set the Working Set back to default (sys.path).
 
-def setDefaultWorkingSet():
-    """Set the Working Set back to default (sys.path).
+        The Working Set is the active module path where your modules for a 
+        project reside.
+        """
+        self.wset = pkg_resources.working_set
 
-    The Working Set is the active module path where your modules for a 
-    project reside.
-    """
-    _working_set = None
+    def getInstallStrings(self):
+        """Parses the output of installFor().
 
-def getInstallStrings():
-    """Parses the output of installFor().
+        Get a Human Readable String which gives Install / uninstall advice.
+        """
+        solver.installRecommendation(self.installList, self.uninstallList)
 
-    Get a Human Readable String which gives Install / uninstall advice.
-    """
-    solver.installRecommendation(_installList, _uninstallList)
+    def drawPNG(self):
+        """Output a .png file with the dependency graph after parsed module would be installed.
 
-def drawPNG():
-    """Output a .png file with the dependency graph after parsed module would be installed.
+        A module should have been parsed with installFor() before calling this function
+        """
+        graph = solver.getFutureState(self.installList)
+        depgraph.graphToPNG(graph)
 
-    A module should have been parsed with installFor() before calling this function
-    """
-    graph = solver.getFutureState(_installList)
-    depgraph.graphToPNG(graph)
+    def drawSVG(self):
+        """Output a .svg file with the dependency graph after parsed module would be installed.
 
-def drawSVG():
-    """Output a .svg file with the dependency graph after parsed module would be installed.
+        A module should have been parsed with installFor() before calling this function
+        graphviz package has to be installed on UNIX Systems.
+        """
+        graph = solver.getFutureState(self.installList)
+        depgraph.graphToSVG(graph)
 
-    A module should have been parsed with installFor() before calling this function
-    graphviz package has to be installed on UNIX Systems.
-    """
-    graph = solver.getFutureState(_installList)
-    depgraph.graphToSVG(graph)
+    def isSolvable(self):
+        """Returns wether the current instance is satisfiable or not. (Run InstallFor() beforehand)"""
 
-def isSolvable():
-    """Returns wether the current instance is satisfiable or not. (Run InstallFor() beforehand)"""
-
-    return _solvable 
+        return self.solvable 
 
 def checkFutureDependency(module, depname):
     """Check if dependencies of module keep consistent after adding depname
